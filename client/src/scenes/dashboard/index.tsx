ed725a3787de1ee6f809a45df38baa1a7122f1b7
Vansh Thapar <thaparvansh29@gmail.com>
2024-03-18 14:31:03 +0530

    Starting setup using vite and grid layout for dashboard

---
+++ client/src/scenes/dashboard/index.tsx
@@ -0,0 +1,84 @@
+import { Box, useMediaQuery, useTheme } from "@mui/material"
+
+const gridTemplateLargeScreen = `
+ "a b c"
+ "a b c"
+ "a b c"
+ "a b f"
+ "d e f"
+ "d e f"
+ "d h i"
+ "g h i"
+ "g h j"
+ "g h j"
+`
+
+// for responsiveness 
+const gridTemplateSmallScreen = `
+"a"
+"a"
+"a"
+"a"
+"b"
+"b"
+"b"
+"b"
+"c"
+"c"
+"c"
+"d"
+"d"
+"d"
+"e"
+"e"
+"f"
+"f"
+"f"
+"g"
+"g"
+"g"
+"h"
+"h"
+"h"
+"h"
+"i"
+"i"
+"j"
+"j"
+`
+
+const Dashboard = () => {
+    const isAboveMediumScreens = useMediaQuery("(min-width:1200px)")
+    const { palette } = useTheme()
+
+    return (
+        <Box width="100%" height="100%" display="grid" gap="1.5rem"
+            sx={isAboveMediumScreens ?
+                {
+                    gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+                    gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+                    gridTemplateAreas: gridTemplateLargeScreen
+                }
+                :
+                {
+                    gridAutoColumns: "1fr",
+                    gridAutoRows: "80px",
+                    gridTemplateAreas: gridTemplateSmallScreen
+                }
+            }
+        >
+            <Box bgcolor="#fff" gridArea="a"></Box>
+            <Box bgcolor="#fff" gridArea="b"></Box>
+            <Box bgcolor="#fff" gridArea="c"></Box>
+            <Box bgcolor="#fff" gridArea="d"></Box>
+            <Box bgcolor="#fff" gridArea="e"></Box>
+            <Box bgcolor="#fff" gridArea="f"></Box>
+            <Box bgcolor="#fff" gridArea="g"></Box>
+            <Box bgcolor="#fff" gridArea="h"></Box>
+            <Box bgcolor="#fff" gridArea="i"></Box>
+            <Box bgcolor="#fff" gridArea="j"></Box>
+        </Box >
+    )
+}
+
+export default Dashboard
